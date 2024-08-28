5f00e7262b9e9b4cb0d56221f1c5df558c75330d
RicoAraujo <ricardoaraujo038@gmail.com>
2024-08-28 20:35:14 -0300

    Desafio Funções DIO

---
+++ index.js
@@ -0,0 +1,24 @@
+let resultado = soma(70, 11)
+let rankAtual = ""
+statusPlayer()
+ if (soma === 11 <= 20) { console.log (rankAtual = "bronze") }
+else if (soma === 21 <= 50) {console.log (rankAtual = "prata") }
+else (soma === 51 <= 80); {console.log (rankAtual = "ouro") }
+    
+
+
+function statusPlayer(){
+    console.log("O heroi tem o saldo de " + soma + " está no nível de " + (rankAtual = "") )
+         
+        
+}
+
+
+function soma(numA, numB) {
+    soma = (numA - numB)
+         return soma
+
+   
+}
+
+
